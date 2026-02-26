5ff0bfdd077b59eff35d518f78fd93da7bd1c29d
Bill Chen <bill.hongwu.chen@gmail.com>
2026-02-26 14:47:48 -0500

    add more code coverage

--- src/test/java/com/training/example/jacocoexample/PalindromeTest.java
+++ src/test/java/com/training/example/jacocoexample/PalindromeTest.java
@@ -8,9 +8,60 @@ import com.training.example.jacocoexample.Palindrome;
 
 public class PalindromeTest {
 
+	private Palindrome palindromeTester = new Palindrome();
+
 	@Test
 	public void whenEmptyString_thenAccept() {
-	    Palindrome palindromeTester = new Palindrome();
 	    assertTrue(palindromeTester.isPalindrome(""));
+	}
+	
+	@Test
+	public void whenSingleCharacter_thenAccept() {
+	    assertTrue(palindromeTester.isPalindrome("a"));
+	}
+	
+	@Test
+	public void whenSimplePalindrome_thenAccept() {
+	    assertTrue(palindromeTester.isPalindrome("aba"));
+	}
+	
+	@Test
+	public void whenLongerPalindrome_thenAccept() {
+	    assertTrue(palindromeTester.isPalindrome("racecar"));
+	}
+	
+	@Test
+	public void whenEvenLengthPalindrome_thenAccept() {
+	    assertTrue(palindromeTester.isPalindrome("noon"));
+	}
+	
+	@Test
+	public void whenSingleCharNonPalindrome_thenReject() {
+	    assertTrue(palindromeTester.isPalindrome("a"));
+	}
+	
+	@Test
+	public void whenTwoCharNonPalindrome_thenReject() {
+	    assertFalse(palindromeTester.isPalindrome("ab"));
+	}
+	
+	@Test
+	public void whenSimpleNonPalindrome_thenReject() {
+	    assertFalse(palindromeTester.isPalindrome("hello"));
+	}
+	
+	@Test
+	public void whenLongerNonPalindrome_thenReject() {
+	    assertFalse(palindromeTester.isPalindrome("world"));
+	}
+	
+	@Test
+	public void whenAllSameCharacters_thenAccept() {
+	    assertTrue(palindromeTester.isPalindrome("aaaa"));
+	}
+	
+	@Test
+	public void whenTwoCharacterPalindrome_thenAccept() {
+	    assertTrue(palindromeTester.isPalindrome("aa"));
 	}
 }
